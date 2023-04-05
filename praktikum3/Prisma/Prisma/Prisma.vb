331984871307fe2b8b54f379cad61bb9e2be2925
YediRisdianto <93753659+YediRisdianto@users.noreply.github.com>
2023-04-05 21:10:41 +0700

    Add files via upload

---
+++ praktikum3/Prisma/Prisma/Prisma.vb
@@ -0,0 +1,29 @@
+﻿Public Class Prisma
+    Private Function HitungVol(luasAlas As Integer, tinggi As Integer) As Integer
+        Dim volume As Integer
+        volume = luasAlas * tinggi
+        Return volume
+    End Function
+
+    Private Function HitungLP(tinggi As Integer, kelilingAlas As Integer, luasAlas As Integer) As Integer
+        Dim luasPermukaan As Integer
+        luasPermukaan = (tinggi * kelilingAlas) + (2 * luasAlas)
+        Return luasPermukaan
+    End Function
+
+    Private Sub btnHitung_Click(sender As Object, e As EventArgs) Handles btnHitung.Click
+        Dim luasAlas, kelilingAlas, tinggi, volume, luasPermukaan As Integer
+
+        luasAlas = Val(txtLuasAlas.Text)
+        kelilingAlas = Val(txtKelilingAlas.Text)
+        tinggi = Val(txtTinggi.Text)
+
+        volume = HitungVol(luasAlas, tinggi)
+        luasPermukaan = HitungLP(tinggi, kelilingAlas, luasAlas)
+
+        txtVolume.Text = Str(volume)
+        txtLuasPermukaan.Text = Str(luasPermukaan)
+
+    End Sub
+
+End Class
---
+++ praktikum3/Tabung/Tabung/Tabung.vb
@@ -0,0 +1,29 @@
+﻿Public Class Tabung
+
+    Private Function HitungVol(jari As Integer, tinggi As Integer) As Integer
+        Dim volume As Integer
+        volume = 3.14 * jari * jari * tinggi
+        Return volume
+    End Function
+
+    Private Function HitungLP(jari As Integer, tinggi As Integer) As Integer
+        Dim luasPermukaan As Integer
+        luasPermukaan = 2 * 3.14 * jari * (jari + tinggi)
+        Return luasPermukaan
+    End Function
+
+    Private Sub btnHitung_Click(sender As Object, e As EventArgs) Handles btnHitung.Click
+        Dim jari, tinggi, volume, luasPermukaan As Integer
+
+        jari = Val(txtJari.Text)
+        tinggi = Val(txtTinggi.Text)
+
+        volume = HitungVol(jari, tinggi)
+        luasPermukaan = HitungLP(jari, tinggi)
+
+        txtVolume.Text = Str(volume)
+        txtLuasPermukaan.Text = Str(luasPermukaan)
+
+    End Sub
+
+End Class
