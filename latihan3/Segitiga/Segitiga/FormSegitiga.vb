e11b447f914f75f5ffad19feac893ba6c00aa2b9
YediRisdianto <93753659+YediRisdianto@users.noreply.github.com>
2023-04-05 21:06:19 +0700

    Add files via upload

---
+++ latihan3/Segitiga/Segitiga/FormSegitiga.vb
@@ -0,0 +1,20 @@
+﻿Public Class FormSegitiga
+    Private Sub btnHitung_Click(sender As Object, e As EventArgs) Handles btnHitung.Click
+
+        Dim sisiA, sisiB, sisiC, alas, tinggi, luas, keliling As Integer
+
+        sisiA = Val(txtSisiA.Text)
+        sisiB = Val(txtSisiB.Text)
+        sisiC = Val(txtSisiC.Text)
+        alas = Val(txtAlas.Text)
+        tinggi = Val(txtTinggi.Text)
+
+        luas = 0.5 * alas * tinggi
+        keliling = sisiA + sisiB + sisiC
+
+        txtLuas.Text = Str(luas)
+        txtKeliling.Text = Str(keliling)
+
+
+    End Sub
+End Class
