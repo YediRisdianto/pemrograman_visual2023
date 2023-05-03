16d1c7cec93124a19567107ee6c8d717e32be049
YediRisdianto <93753659+YediRisdianto@users.noreply.github.com>
2023-05-03 21:58:08 +0700

    Add files via upload

---
+++ tugas6/checkbox/checkbox/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Private Sub btnHitung_Click(sender As Object, e As EventArgs) Handles btnHitung.Click
+
+        Dim hargaBarang As Double
+        Dim jumlahBarang As Integer
+        Dim totalbyr As Double
+
+        hargaBarang = CDbl(txtHarga.Text)
+        jumlahBarang = CInt(txtJumlah.Text)
+        totalbyr = hargaBarang * jumlahBarang
+        If checkMember.Checked Then
+            totalbyr *= 0.9 'diskon 10% untuk anggota
+        End If
+
+        txtTotal.Text = Format(totalbyr, "N0")
+    End Sub
+
+End Class
---
+++ tugas6/radiobutton/radiobutton/Form1.Designer.vb
@@ -0,0 +1,152 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtHargaTiket = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.radioDewasa = New System.Windows.Forms.RadioButton()
+        Me.radioAnak = New System.Windows.Forms.RadioButton()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(40, 39)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(274, 15)
+        Me.Label4.TabIndex = 15
+        Me.Label4.Text = "Pemesanan Masuk Tiket Wisata Kebun Binatang"
+        '
+        'txtHargaTiket
+        '
+        Me.txtHargaTiket.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtHargaTiket.Location = New System.Drawing.Point(154, 209)
+        Me.txtHargaTiket.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtHargaTiket.Name = "txtHargaTiket"
+        Me.txtHargaTiket.Size = New System.Drawing.Size(87, 21)
+        Me.txtHargaTiket.TabIndex = 13
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(60, 212)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(70, 15)
+        Me.Label2.TabIndex = 12
+        Me.Label2.Text = "Harga Tiket"
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.BackColor = System.Drawing.Color.White
+        Me.btnSubmit.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSubmit.Location = New System.Drawing.Point(154, 154)
+        Me.btnSubmit.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(87, 26)
+        Me.btnSubmit.TabIndex = 11
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = False
+        '
+        'radioDewasa
+        '
+        Me.radioDewasa.AutoSize = True
+        Me.radioDewasa.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.radioDewasa.Location = New System.Drawing.Point(154, 118)
+        Me.radioDewasa.Margin = New System.Windows.Forms.Padding(2)
+        Me.radioDewasa.Name = "radioDewasa"
+        Me.radioDewasa.Size = New System.Drawing.Size(70, 19)
+        Me.radioDewasa.TabIndex = 10
+        Me.radioDewasa.TabStop = True
+        Me.radioDewasa.Text = "Dewasa"
+        Me.radioDewasa.UseVisualStyleBackColor = True
+        '
+        'radioAnak
+        '
+        Me.radioAnak.AutoSize = True
+        Me.radioAnak.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.radioAnak.Location = New System.Drawing.Point(154, 95)
+        Me.radioAnak.Margin = New System.Windows.Forms.Padding(2)
+        Me.radioAnak.Name = "radioAnak"
+        Me.radioAnak.Size = New System.Drawing.Size(83, 19)
+        Me.radioAnak.TabIndex = 9
+        Me.radioAnak.TabStop = True
+        Me.radioAnak.Text = "Anak-Anak"
+        Me.radioAnak.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(60, 95)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(78, 15)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Pilih kategori"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(173, 265)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(123, 13)
+        Me.Label3.TabIndex = 16
+        Me.Label3.Text = "design by Yedi Risdianto"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(364, 317)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtHargaTiket)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.radioDewasa)
+        Me.Controls.Add(Me.radioAnak)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtHargaTiket As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents radioDewasa As System.Windows.Forms.RadioButton
+    Friend WithEvents radioAnak As System.Windows.Forms.RadioButton
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label3 As Label
+End Class
