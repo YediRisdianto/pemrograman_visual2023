3cf751f4b65ad2428de640527001b4fc0e789f21
YediRisdianto <93753659+YediRisdianto@users.noreply.github.com>
2023-07-20 11:45:24 +0700

    Add files via upload

---
+++ Pertemuan4/tugas4/ConvertReamur/ConvertReamur/ConvertReamur.vb
@@ -0,0 +1,33 @@
+﻿Public Class ConvertReamur
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+
+        Dim R, C, F, K As Decimal
+        R = CDec(txtReamur.Text)
+        C = ToCelcius(R)
+        F = ToFahrenheit(R)
+        K = ToKelvin(R)
+
+        txtCelcius.Text = Str(C)
+        txtFahrenheit.Text = Str(F)
+        txtKelvin.Text = Str(K)
+    End Sub
+
+    Private Function ToCelcius(suhu As Decimal) As Decimal
+        Dim C As Decimal
+        C = (5 / 4) * suhu
+        Return C
+    End Function
+
+    Private Function ToFahrenheit(suhu As Decimal) As Decimal
+        Dim F As Decimal
+        F = (9 / 4) * suhu + 32
+        Return F
+    End Function
+
+    Private Function ToKelvin(suhu As Decimal) As Decimal
+        Dim K As Decimal
+        K = (5 / 4 * suhu) + 273
+        Return K
+    End Function
+
+End Class
