b39797c53b0889f58cd46d1d6199c4fd487a6ccd
YediRisdianto <93753659+YediRisdianto@users.noreply.github.com>
2023-07-20 11:34:55 +0700

    Add files via upload

---
+++ Pertemuan5/latihan5/Percabangan/Percabangan/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+
+        If (Val(txtMasaKerja.Text) >= 20) Then
+            txtKeterangan.Text = "Anda mendapat bonus liburan ke lombok"
+
+        ElseIf (txtMasaKerja.Text >= 15) Then
+            txtKeterangan.Text = "Anda mendapat bonus mobil"
+
+        ElseIf (txtMasaKerja.Text >= 10) Then
+            txtKeterangan.Text = "Anda mendapat bonus motor"
+
+        ElseIf (txtMasaKerja.Text >= 5) Then
+            txtKeterangan.Text = "Anda mendapat voucher belanja 1 Juta Rupiah"
+
+        Else
+            txtKeterangan.Text = "Maaf anda belum layak dapat bonus"
+        End If
+
+    End Sub
+End Class
---
+++ Pertemuan5/latihan5/Percabangan/Percabangan/Form3.Designer.vb
@@ -0,0 +1,113 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form3
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtKeterangan = New System.Windows.Forms.TextBox()
+        Me.txtKodeJabatan = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Poppins", 6.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(196, 164)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(137, 16)
+        Me.Label3.TabIndex = 17
+        Me.Label3.Text = "create by Yedi Risdianto 2023"
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Font = New System.Drawing.Font("Poppins", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSubmit.Location = New System.Drawing.Point(141, 52)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(75, 23)
+        Me.btnSubmit.TabIndex = 16
+        Me.btnSubmit.Text = "submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Poppins", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(22, 87)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(79, 22)
+        Me.Label2.TabIndex = 15
+        Me.Label2.Text = "Keterangan"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Poppins", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(22, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(93, 22)
+        Me.Label1.TabIndex = 14
+        Me.Label1.Text = "Kode Jabatan"
+        '
+        'txtKeterangan
+        '
+        Me.txtKeterangan.Font = New System.Drawing.Font("Poppins", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtKeterangan.Location = New System.Drawing.Point(26, 112)
+        Me.txtKeterangan.Name = "txtKeterangan"
+        Me.txtKeterangan.Size = New System.Drawing.Size(281, 24)
+        Me.txtKeterangan.TabIndex = 13
+        '
+        'txtKodeJabatan
+        '
+        Me.txtKodeJabatan.Font = New System.Drawing.Font("Poppins", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtKodeJabatan.Location = New System.Drawing.Point(26, 52)
+        Me.txtKodeJabatan.Name = "txtKodeJabatan"
+        Me.txtKodeJabatan.Size = New System.Drawing.Size(100, 24)
+        Me.txtKodeJabatan.TabIndex = 12
+        '
+        'Form3
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(348, 196)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtKeterangan)
+        Me.Controls.Add(Me.txtKodeJabatan)
+        Me.Name = "Form3"
+        Me.Text = "Form3"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtKeterangan As TextBox
+    Friend WithEvents txtKodeJabatan As TextBox
+End Class
