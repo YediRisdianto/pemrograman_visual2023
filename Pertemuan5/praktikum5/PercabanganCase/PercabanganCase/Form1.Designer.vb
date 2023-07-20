4011039cfd09e919eab6dcd004baf9da45218761
YediRisdianto <93753659+YediRisdianto@users.noreply.github.com>
2023-07-20 11:37:37 +0700

    Add files via upload

---
+++ Pertemuan5/praktikum5/PercabanganCase/PercabanganCase/Form1.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtKeterangan = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtKendaraan = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Poppins", 6.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(198, 133)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(129, 16)
+        Me.Label3.TabIndex = 17
+        Me.Label3.Text = "Copyright By Yedi Risdianto"
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(225, 40)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(75, 23)
+        Me.btnSubmit.TabIndex = 16
+        Me.btnSubmit.Text = "submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Poppins", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(25, 93)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(79, 22)
+        Me.Label2.TabIndex = 15
+        Me.Label2.Text = "Keterangan"
+        '
+        'txtKeterangan
+        '
+        Me.txtKeterangan.Font = New System.Drawing.Font("Poppins", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtKeterangan.Location = New System.Drawing.Point(110, 90)
+        Me.txtKeterangan.Name = "txtKeterangan"
+        Me.txtKeterangan.Size = New System.Drawing.Size(190, 25)
+        Me.txtKeterangan.TabIndex = 14
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Poppins", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(25, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(76, 22)
+        Me.Label1.TabIndex = 13
+        Me.Label1.Text = "Kendaraan"
+        '
+        'txtKendaraan
+        '
+        Me.txtKendaraan.Font = New System.Drawing.Font("Poppins", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtKendaraan.Location = New System.Drawing.Point(110, 38)
+        Me.txtKendaraan.Name = "txtKendaraan"
+        Me.txtKendaraan.Size = New System.Drawing.Size(109, 25)
+        Me.txtKendaraan.TabIndex = 12
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(337, 165)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtKeterangan)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtKendaraan)
+        Me.Name = "Form1"
+        Me.Text = "Kendaraan"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtKeterangan As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtKendaraan As TextBox
+End Class
